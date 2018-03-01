3ecefec1f9b5856f23461cbcdd8590d82b6c30aa
kryseno <kryseno@gmail.com>
2018-03-01 02:30:31 -0800

    [added] channel name & a tag

--- simpleXMLpractice.php
+++ simpleXMLpractice.php
@@ -2,10 +2,11 @@
 $html = '';
 $url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCD5WWnRed32y3xGwmrDhUiQ';
 $xml = simplexml_load_file($url);
-
+$channel = $xml->title;
+$address = $xml->author->uri;
 for($i=0; $i<10; $i++){
     $title = $xml->entry[$i]->title;
     $html .= "<h3>$title</h3><hr>";
 }
-echo $html
+echo "<h1>YouTube Channel: <a href='$address'>$channel</a></h1>".$html
 ?>
