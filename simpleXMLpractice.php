15931058cabc421da28d8d852039b7dfd558fbfb
kryseno <kryseno@gmail.com>
2018-03-01 03:11:57 -0800

    [added] video description

--- simpleXMLpractice.php
+++ simpleXMLpractice.php
@@ -10,10 +10,12 @@ for($i=0; $i<10; $i++){
     $title = $xml->entry[$i]->title;
     $videoId = $xml->entry[$i]->id;
     $videoId = str_replace('yt:video:', '', $videoId);
+    $media = $xml->entry[$i]->children('http://search.yahoo.com/mrss/');
+    $description = $media->group->description;
+    $description = substr($description, 0, strrpos($description, 'SUBSCRIBE'));
     $html .= "<h3>$title</h3>
-    <br>
-    <iframe width='560' height='315' src='https://www.youtube.com/embed/$videoId' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>
-    <hr>";
+    <br><iframe width='560' height='315' src='https://www.youtube.com/embed/$videoId' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>
+    <br>$description<hr>";
 }
 echo $header.$html
 ?>
