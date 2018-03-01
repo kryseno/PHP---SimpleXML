ea55d76912ee5f9a916116793a0b96c0e2eb7fbe
kryseno <kryseno@gmail.com>
2018-03-01 02:43:32 -0800

    [added] youtube video; [updated] arrangement of code

--- simpleXMLpractice.php
+++ simpleXMLpractice.php
@@ -4,9 +4,16 @@ $url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCD5WWnRed32y3xGwmrD
 $xml = simplexml_load_file($url);
 $channel = $xml->title;
 $address = $xml->author->uri;
+$header = "<h1>YouTube Channel: <a href='$address'>$channel</a></h1>";
+
 for($i=0; $i<10; $i++){
     $title = $xml->entry[$i]->title;
-    $html .= "<h3>$title</h3><hr>";
+    $videoId = $xml->entry[$i]->id;
+    $videoId = str_replace('yt:video:', '', $videoId);
+    $html .= "<h3>$title</h3>
+    <br>
+    <iframe width='560' height='315' src='https://www.youtube.com/embed/$videoId' frameborder='0' allow='autoplay; encrypted-media' allowfullscreen></iframe>
+    <hr>";
 }
-echo "<h1>YouTube Channel: <a href='$address'>$channel</a></h1>".$html
+echo $header.$html
 ?>
