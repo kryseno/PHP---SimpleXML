6fb3aecca4b448763b50816a2a7a6d9aff2b1845
kryseno <kryseno@gmail.com>
2018-03-05 23:01:53 -0800

    [updated] using cURL to grab data

--- request.php
+++ request.php
@@ -1,17 +1,24 @@
 <?php
 //*******************//
+//    without cURL   //
+//*******************//
+// $html = '';
+// $url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCD5WWnRed32y3xGwmrDhUiQ';
+// $xml = simplexml_load_file($url); //skips cURL
+// $channel = $xml->title;
+// $address = $xml->author->uri;
+
+//*******************//
 //    cURL example   //
 //*******************//
 $ch = curl_init();
 curl_setopt($ch, CURLOPT_URL, 'https://www.youtube.com/feeds/videos.xml?channel_id=UCD5WWnRed32y3xGwmrDhUiQ');
 curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-$response = curl_Exec($ch);
-//*******************//
-//    without cURL   //
-//*******************//
-$html = '';
-$url = 'https://www.youtube.com/feeds/videos.xml?channel_id=UCD5WWnRed32y3xGwmrDhUiQ';
-$xml = simplexml_load_file($url); //skips cURL
+curl_setopt($ch, CURLOPT_HEADER, 0);
+$response = curl_exec($ch);
+curl_close($ch);
+
+$xml = simplexml_load_string($response);
 $channel = $xml->title;
 $address = $xml->author->uri;
 ?>
